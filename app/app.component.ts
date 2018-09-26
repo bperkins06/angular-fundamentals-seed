206221a43f47250a05f42ea6ade5ca490c94ec9c
bperkins <brian.perkins@adtran.com>
2018-09-26 09:22:58 -0500

    Getting Started: root module.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  styleUrls: ['app.component.scss'],
+  selector: "app-root",
+  styleUrls: ["app.component.scss"],
   template: `
     <div class="app">
-      Hello!
+      {{ title }}
+      Hello World!
     </div>
   `
 })
 export class AppComponent {
-
+  title: string;
+  constructor() {
+    this.title = "Ultimate Angular";
+  }
 }
